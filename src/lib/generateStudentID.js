15851e7ee211c614cd3f85a3b233674ef0bb73be
ASOT1201 <Apostolicsot@gmail.com>
2025-12-05 01:43:06 -0600

    Update generateStudentID.js

--- src/lib/generateStudentID.js
+++ src/lib/generateStudentID.js
@@ -1,1 +1,16 @@
+export function generateStudentID(prefix = '', last4 = '', useSSN = false){
+  function randLetters(n = 2){
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    return Array.from({length:n}, () => chars[Math.floor(Math.random()*chars.length)]).join('')
+  }
+  function randDigits(n = 4){
+    return Math.floor(Math.random()*Math.pow(10,n)).toString().padStart(n,'0')
+  }
+
+  if(useSSN && prefix.length >= 2 && /^[0-9]{4}$/.test(last4)){
+    return prefix.slice(0,2).toUpperCase() + last4
+  }
+  const letters = prefix && prefix.length >= 2 ? prefix.slice(0,2).toUpperCase() : randLetters()
+  return letters + randDigits()
+}
 
